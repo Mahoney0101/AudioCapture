b5cfb21f77b22b0ca61144ebe8a6af9c29bbad11
Mahoney0101 <hoooch.mahoney@gmail.com>
2020-12-05 17:35:21 +0000

    runs audio spectrogram through CNN model.

--- audio_capture.py
+++ audio_capture.py
@@ -3,6 +3,27 @@ import wave
 import os
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
+from tensorflow import keras
+import numpy as np
+from matplotlib import image
+
+model = keras.models.load_model('/home/james/Downloads/CNNModel.h5')
+# wheezes = load('/home/james/Downloads/wheezedata.npy', allow_pickle=True)
+# both = load('/home/james/Downloads/bothdata.npy', allow_pickle=True)
+# crackles = load('/home/james/Downloads/cracklesdata.npy', allow_pickle=True)
+# none = load('/home/james/Downloads/data.npy', allow_pickle=True)
+clip = []
+# clip.append(none[1][0].reshape(50, 245, 1))
+# clip.append(wheezes[1][0].reshape(50, 245, 1))
+# clip.append(wheezes[2][0].reshape(50, 245, 1))
+# clip.append(wheezes[2][0].reshape(50, 245, 1))
+# clip.append(both[8][0].reshape(50, 245, 1))
+# clip.append(both[4][0].reshape(50, 245, 1))
+# clip.append(none[5][0].reshape(50, 245, 1))
+# clip.append(crackles[8][0].reshape(50, 245, 1))
+# clip.append(crackles[4][0].reshape(50, 245, 1))
+# clip.append(crackles[5][0].reshape(50, 245, 1))
+
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
@@ -19,7 +40,7 @@ def graph_spectrogram(wav_file):
     ax.axis('off')
     pxx, freqs, bins, im = ax.specgram(x=data, Fs=rate, noverlap=384, NFFT=512)
     ax.axis('off')
-    fig.savefig('sp_xyz.png', dpi=300, frameon='false')
+    fig.savefig('sp_xyz.png', dpi=300)
 
 
 def record():
@@ -55,3 +76,13 @@ audio_fpath = "resources/"
 audio_clips = os.listdir(audio_fpath)
 print("No. of .wav files in audio folder = ", len(audio_clips))
 graph_spectrogram('resources/test.wav')
+
+image = image.imread('sp_xyz.png')
+
+audio_clip = image.resize(50, 245, 1)
+audio_clip = np.array(image, dtype=np.float32)
+clip.append(audio_clip.reshape(1, 50, 245, 1))
+
+prediction = model.predict(clip)
+classes = np.argmax(prediction, axis=1)
+print(classes)
