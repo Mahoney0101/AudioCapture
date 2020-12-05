2ed5c97616d78eb6c4e6ade5ee6c2371b25ecec0
Mahoney0101 <hoooch.mahoney@gmail.com>
2020-12-05 14:11:14 +0000

    record audio, create spectrogram

--- audio_capture.py
+++ audio_capture.py
@@ -1,33 +1,57 @@
 import pyaudio
 import wave
- 
+import os
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
+CHANNELS = 1
+RATE = 22000
 CHUNK = 1024
 RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "file.wav"
- 
-audio = pyaudio.PyAudio()
- 
-stream = audio.open(format=FORMAT, channels=CHANNELS,
-                rate=RATE, input=True,
-                frames_per_buffer=CHUNK)
-print "recording..."
-frames = []
- 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
-print "finished recording"
- 
-stream.stop_stream()
-stream.close()
-audio.terminate()
- 
-waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-waveFile.setnchannels(CHANNELS)
-waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-waveFile.setframerate(RATE)
-waveFile.writeframes(b''.join(frames))
-waveFile.close()
+WAVE_OUTPUT_FILENAME = "resources/test.wav"
+
+
+def graph_spectrogram(wav_file):
+    rate, data = wavfile.read(wav_file)
+    fig, ax = plt.subplots(1)
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    ax.axis('off')
+    pxx, freqs, bins, im = ax.specgram(x=data, Fs=rate, noverlap=384, NFFT=512)
+    ax.axis('off')
+    fig.savefig('sp_xyz.png', dpi=300, frameon='false')
+
+
+def record():
+    audio = pyaudio.PyAudio()
+
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,
+                        frames_per_buffer=CHUNK)
+    print("recording...")
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    print("finished recording")
+
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    wave_file = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wave_file.setnchannels(CHANNELS)
+    wave_file.setsampwidth(audio.get_sample_size(FORMAT))
+    wave_file.setframerate(RATE)
+    wave_file.writeframes(b''.join(frames))
+    wave_file.close()
+
+    return wave_file
+
+
+audio = record()
+audio_fpath = "resources/"
+audio_clips = os.listdir(audio_fpath)
+print("No. of .wav files in audio folder = ", len(audio_clips))
+graph_spectrogram('resources/test.wav')
