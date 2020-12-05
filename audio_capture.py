8dd8409985fc4419ed277027a9f17086847aed68
Mahoney0101 <hoooch.mahoney@gmail.com>
2020-12-05 21:53:59 +0000

    messing with librosa melspectrogram

--- audio_capture.py
+++ audio_capture.py
@@ -6,24 +6,25 @@ from scipy.io import wavfile
 from tensorflow import keras
 import numpy as np
 from matplotlib import image
+import librosa
+import librosa.display
 
 model = keras.models.load_model('/home/james/Downloads/CNNModel.h5')
-# wheezes = load('/home/james/Downloads/wheezedata.npy', allow_pickle=True)
-# both = load('/home/james/Downloads/bothdata.npy', allow_pickle=True)
-# crackles = load('/home/james/Downloads/cracklesdata.npy', allow_pickle=True)
-# none = load('/home/james/Downloads/data.npy', allow_pickle=True)
+wheezes = np.load('/home/james/Downloads/wheezedata.npy', allow_pickle=True)
+both = np.load('/home/james/Downloads/bothdata.npy', allow_pickle=True)
+crackles = np.load('/home/james/Downloads/cracklesdata.npy', allow_pickle=True)
+none = np.load('/home/james/Downloads/data.npy', allow_pickle=True)
 clip = []
-# clip.append(none[1][0].reshape(50, 245, 1))
-# clip.append(wheezes[1][0].reshape(50, 245, 1))
-# clip.append(wheezes[2][0].reshape(50, 245, 1))
-# clip.append(wheezes[2][0].reshape(50, 245, 1))
-# clip.append(both[8][0].reshape(50, 245, 1))
-# clip.append(both[4][0].reshape(50, 245, 1))
-# clip.append(none[5][0].reshape(50, 245, 1))
-# clip.append(crackles[8][0].reshape(50, 245, 1))
-# clip.append(crackles[4][0].reshape(50, 245, 1))
-# clip.append(crackles[5][0].reshape(50, 245, 1))
-
+clip.append(none[1][0].reshape(50, 245, 1))
+clip.append(wheezes[1][0].reshape(50, 245, 1))
+clip.append(wheezes[2][0].reshape(50, 245, 1))
+clip.append(wheezes[2][0].reshape(50, 245, 1))
+clip.append(both[8][0].reshape(50, 245, 1))
+clip.append(both[4][0].reshape(50, 245, 1))
+clip.append(none[5][0].reshape(50, 245, 1))
+clip.append(crackles[8][0].reshape(50, 245, 1))
+clip.append(crackles[4][0].reshape(50, 245, 1))
+clip.append(crackles[5][0].reshape(50, 245, 1))
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
@@ -34,13 +35,34 @@ WAVE_OUTPUT_FILENAME = "resources/test.wav"
 
 
 def graph_spectrogram(wav_file):
-    rate, data = wavfile.read(wav_file)
+    filename = 'resources/test.wav'
+    y, sr = librosa.load(filename)
+    # trim silent edges
+    test, _ = librosa.effects.trim(y)
+    n_mels = 1
+    n_fft = 512
+    D = np.abs(librosa.stft(test[:n_fft], n_fft=n_fft, hop_length=n_fft + 1))
+    plt.plot(D)
+    hop_length = 512
+    # D = np.abs(librosa.stft(test, n_fft=n_fft, hop_length=hop_length))
+    # librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='linear');
+    # DB = librosa.amplitude_to_db(D, ref=np.max)
+    # librosa.display.specshow(DB, sr=sr, hop_length=hop_length, x_axis='time', y_axis='log');
+
+    plt.figure(figsize=(20, 10))
+    mel = librosa.filters.mel(sr=sr, n_fft=n_fft, n_mels=n_mels)
+
+    # mel_10 = librosa.filters.mel(sr=sr, n_fft=n_fft, n_mels=10)
+    # librosa.display.specshow(mel_10, sr=sr, hop_length=hop_length, x_axis='linear');
+    plt.tight_layout()
     fig, ax = plt.subplots(1)
     fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
     ax.axis('off')
-    pxx, freqs, bins, im = ax.specgram(x=data, Fs=rate, noverlap=384, NFFT=512)
-    ax.axis('off')
-    fig.savefig('sp_xyz.png', dpi=300)
+    plt.axis('off')
+    S = librosa.feature.melspectrogram(test, sr=sr, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
+    S_DB = librosa.power_to_db(S, ref=np.max)
+    librosa.display.specshow(S_DB, sr=sr, hop_length=hop_length, x_axis='time', y_axis='mel')
+    plt.savefig('sp_xyz.png', dpi=300)
 
 
 def record():
@@ -81,7 +103,8 @@ image = image.imread('sp_xyz.png')
 
 audio_clip = image.resize(50, 245, 1)
 audio_clip = np.array(image, dtype=np.float32)
-clip.append(audio_clip.reshape(1, 50, 245, 1))
+clip.append(audio_clip.reshape(50, 245, 1))
+clip = np.array(clip)
 
 prediction = model.predict(clip)
 classes = np.argmax(prediction, axis=1)
