3c45055628cd19a6d42da89d8a963b13b969fa0d
Mahoney0101 <hoooch.mahoney@gmail.com>
2021-02-15 22:18:30 +0000

    Update

--- audio_capture.py
+++ audio_capture.py
@@ -1,73 +1,50 @@
 import pyaudio
 import wave
-import os
 import matplotlib.pyplot as plt
-from scipy.io import wavfile
 from tensorflow import keras
 import numpy as np
-from matplotlib import image
 import librosa
 import librosa.display
+import soundfile as sf
 
-model = keras.models.load_model('/home/james/Downloads/CNNModel.h5')
-wheezes = np.load('/home/james/Downloads/wheezedata.npy', allow_pickle=True)
-both = np.load('/home/james/Downloads/bothdata.npy', allow_pickle=True)
-crackles = np.load('/home/james/Downloads/cracklesdata.npy', allow_pickle=True)
-none = np.load('/home/james/Downloads/data.npy', allow_pickle=True)
-clip = []
-clip.append(none[1][0].reshape(50, 245, 1))
-clip.append(wheezes[1][0].reshape(50, 245, 1))
-clip.append(wheezes[2][0].reshape(50, 245, 1))
-clip.append(wheezes[2][0].reshape(50, 245, 1))
-clip.append(both[8][0].reshape(50, 245, 1))
-clip.append(both[4][0].reshape(50, 245, 1))
-clip.append(none[5][0].reshape(50, 245, 1))
-clip.append(crackles[8][0].reshape(50, 245, 1))
-clip.append(crackles[4][0].reshape(50, 245, 1))
-clip.append(crackles[5][0].reshape(50, 245, 1))
+model = keras.models.load_model('/home/james/Downloads/Jan21RespModel.h5')
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 22000
-CHUNK = 1024
+CHUNK = 512
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "resources/test.wav"
 
 
-def graph_spectrogram(wav_file):
-    filename = 'resources/test.wav'
-    y, sr = librosa.load(filename)
-    # trim silent edges
-    test, _ = librosa.effects.trim(y)
-    n_mels = 1
-    n_fft = 512
-    D = np.abs(librosa.stft(test[:n_fft], n_fft=n_fft, hop_length=n_fft + 1))
-    plt.plot(D)
-    hop_length = 512
-    # D = np.abs(librosa.stft(test, n_fft=n_fft, hop_length=hop_length))
-    # librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='linear');
-    # DB = librosa.amplitude_to_db(D, ref=np.max)
-    # librosa.display.specshow(DB, sr=sr, hop_length=hop_length, x_axis='time', y_axis='log');
+def audio_features(filename):
+    sound, sample_rate = sf.read(filename)
+    stft = np.abs(librosa.stft(sound))
+    mfccs = np.mean(librosa.feature.mfcc(y=sound, sr=8000, n_mfcc=40, fmin=30).T, axis=0)
+    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=8000).T, axis=0)
+    mel = np.mean(librosa.feature.melspectrogram(sound, sr=8000, fmin=30).T, axis=0)
+    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=8000, fmin=30).T, axis=0)
+    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(sound), sr=sample_rate,
+                                              chroma=librosa.feature.chroma_cqt(y=sound, sr=8000, fmin=30)).T, axis=0)
+    concat = np.concatenate((mfccs, chroma, mel, contrast, tonnetz))
+    return concat
 
-    plt.figure(figsize=(20, 10))
-    mel = librosa.filters.mel(sr=sr, n_fft=n_fft, n_mels=n_mels)
 
-    # mel_10 = librosa.filters.mel(sr=sr, n_fft=n_fft, n_mels=10)
-    # librosa.display.specshow(mel_10, sr=sr, hop_length=hop_length, x_axis='linear');
+def graph_spectrogram(S):
+    plt.subplot(2, 2, 1)
+    fig = plt.figure(frameon=False)
+    fig.set_size_inches(5, 5)
+    librosa.display.specshow(librosa.power_to_db(S, ref=np.max))
     plt.tight_layout()
-    fig, ax = plt.subplots(1)
-    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
-    ax.axis('off')
-    plt.axis('off')
-    S = librosa.feature.melspectrogram(test, sr=sr, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
-    S_DB = librosa.power_to_db(S, ref=np.max)
-    librosa.display.specshow(S_DB, sr=sr, hop_length=hop_length, x_axis='time', y_axis='mel')
-    plt.savefig('sp_xyz.png', dpi=300)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    plt.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[])
+    plt.savefig('sp_xyz.png', bbox_inches=None, pad_inches=0, dpi=300)
 
 
 def record():
     audio = pyaudio.PyAudio()
-
     stream = audio.open(format=FORMAT, channels=CHANNELS,
                         rate=RATE, input=True,
                         frames_per_buffer=CHUNK)
@@ -89,23 +66,17 @@ def record():
     wave_file.setframerate(RATE)
     wave_file.writeframes(b''.join(frames))
     wave_file.close()
-
     return wave_file
 
 
 audio = record()
-audio_fpath = "resources/"
-audio_clips = os.listdir(audio_fpath)
-print("No. of .wav files in audio folder = ", len(audio_clips))
-graph_spectrogram('resources/test.wav')
+x = audio_features('resources/test.wav')
+print(x)
+S = librosa.feature.melspectrogram(x)
+graph_spectrogram(S)
+x = np.array(x)
+x = np.reshape(x, [1, 193, 1])
 
-image = image.imread('sp_xyz.png')
-
-audio_clip = image.resize(50, 245, 1)
-audio_clip = np.array(image, dtype=np.float32)
-clip.append(audio_clip.reshape(50, 245, 1))
-clip = np.array(clip)
-
-prediction = model.predict(clip)
+prediction = model.predict(x)
 classes = np.argmax(prediction, axis=1)
 print(classes)
